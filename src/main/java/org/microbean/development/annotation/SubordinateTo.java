2efdfc42282116d32d2b8a49b47cd6f5acb3f8f9
Laird Nelson <ljnelson@gmail.com>
2021-05-12 15:42:44 -0700

    Added OverridingEncouraged, OverridingDiscouraged and SubordinateTo annotations.
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

---
+++ src/main/java/org/microbean/development/annotation/SubordinateTo.java
@@ -0,0 +1,48 @@
+/* -*- mode: Java; c-basic-offset: 2; indent-tabs-mode: nil; coding: utf-8-unix -*-
+ *
+ * Copyright © 2021 microBean™.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.microbean.development.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates that the annotated element is subordinate to another item in some
+ * way.
+ *
+ * @author <a href="https://about.me/lairdnelson"
+ * target="_parent">Laird Nelson</a>
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({
+  ElementType.METHOD
+})
+public @interface SubordinateTo {
+
+  /**
+   * An identifier of the thing this annotated element is subordinate
+   * to; often a method reference
+   *
+   * @return an identifier of the thing this annotated element is
+   * subordinate to; often a method reference
+   */
+  String value();
+
+}
